3399721f12b48ac5bd8fe339b79bba5029f1ca23
dev_nebula <Joelwoods@hotmail.com>
2022-03-24 23:54:23 +0000

    Fixed SystemIO Ambiguity.

---
+++ Utility/GlobalFunctions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlyEPOS.Utility
+{
+    class GlobalFunctions
+    {
+        /// <summary>
+        /// Try To Open The On-Screen Keyboard Using Windows
+        /// </summary>
+        public static void GetOnScreenKeyboard()
+        {
+        }
+    }
+}
